7c2c836f70a3fea4592a664fc0dfa0beabc5fd2b
Caleb Taylor <caleb89taylor@gmail.com>
2018-02-09 09:08:35 -0800

    fix(install): add oh-my-zsh installation to install script. Fixes #3

--- install.sh
+++ install.sh
@@ -68,6 +68,18 @@ fi
 mkdir -p ~/.local/share/nvim/backup
 
 echo "---------------------------------------------------------"
+echo "$(tput setaf 2)JARVIS: Installing oh-my-zsh.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  echo "---------------------------------------------------------"
+  echo "$(tput setaf 2)JARVIS: oh-my-zsh already installed.$(tput sgr 0)"
+  echo "---------------------------------------------------------"
+fi
+
+echo "---------------------------------------------------------"
 echo "$(tput setaf 2)JARVIS: Installing Neovim plugins and linking dotfiles.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
